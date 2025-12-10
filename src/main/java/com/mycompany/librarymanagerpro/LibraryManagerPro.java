e02df53095c47ea95514aba09c7a45914d3e2808
DGGamer98 <davidegatta03@gmail.com>
2025-12-10 14:27:57 +0100

    Primo commit di questo progetto

---
+++ src/main/java/com/mycompany/librarymanagerpro/LibraryManagerPro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.librarymanagerpro;
+
+/**
+ *
+ * @author david
+ */
+public class LibraryManagerPro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
